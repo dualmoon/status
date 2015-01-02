6d8b6ebcaa21fbce83ca7e230273bcd95dce4b61
Ashley Davis <dual@gmx.com>
2015-01-02 23:19:49 +0000

    Initial

---
+++ web.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'net/http'
+
+set :public_folder, File.dirname(__FILE__) + '/build'
+
+get '/' do
+  redirect '/index.html'
+end
+
+get '/psn' do
+  Net::HTTP.get(URI("https://support.us.playstation.com/app/answers/detail/a_id/237/"))
+end
+
+get '/wow' do
+  Net::HTTP.get(URI("https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=ukm5wf6j68hwj35k57gr7tesgz8pe9k4"))
+end
+
+get '/steam' do
+  Net::HTTP.get(URI("https://steamdb.info/api/SteamRailgun/"))
+end
+
+get '/xbox' do
+  Net::HTTP.get(URI("http://support.xbox.com/en-US/xbox-live-status"))
+end
+
+get '/euf/assets/images/:a' do
+  ''
+end
+
+get '/Content/Images/LiveStatus/:a' do
+  ''
+end
+
+get '/shell/images/shell/:a' do
+  ''
+end
